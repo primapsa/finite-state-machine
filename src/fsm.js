4ac034499acc11412923db4446f1dcc09fcb079a
Siarhei Prymakovich <prima.psa@gmail.com>
2019-10-18 18:42:26 +0300

    feat: implement FSM class

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,84 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.config = config;
+        this.und = [];
+        this.rnd = [];
+        this.indx = 0;
+        if(this.config.initial !== 'normal'){
+            //throw new Error('Initail not Normal');
+            this.config.initial = 'normal';
+        }
+        
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      
+        return this.config.initial;
+    }
+    getrndd ()
+    {
+        return this.rnd;
+    }
+    getInd ()
+    {
+        return this.indx;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        
+        for(let key in this.config.states){
+        
+            if(key === state){
+                this.und.push(this.config.initial);
+                this.config.initial = state;
+                this.rnd.push(state);
+                return
+            }
+        }    
+        
+        throw new Error('Error');
+       
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      
+      let state = this.config.initial;
+      let current = this.config.states;
+
+            for(let inside in current[state].transitions){
+                    if(inside === event ){
+                        this.und.push(state);                       
+                        this.config.initial = current[state].transitions[inside];
+                        this.rnd.push(this.config.initial);                    
+                       
+                        return;
+                    }
+            }
+            throw Error();
+        
+        
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.config.initial = 'normal';
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,27 +88,65 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        let result = []
+        for(let key in this.config.states){ 
+            if(!event) {
+                result.push(key);
+                continue
+            }
+            for(let compare in this.config.states[key].transitions){
+                if(compare == event){
+                    result.push(key);
+                }
+            }
+        }
+        return result;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        
+        if(this.und.length === 0) return false;
+        let current = this.und.pop();      
+        this.indx++;
+        this.config.initial = current;      
+        
+        return true;
+
+        
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        
+        if((this.rnd.length === 0)) return false;
+        if(this.rnd[this.rnd.length-1] === this.config.initial) return false;             
+        let current = this.rnd.reverse();       
+        this.config.initial = current[this.indx-1];      
+        this.indx--;
+        current = this.rnd.reverse();
+
+        return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.rnd = [];
+        this.und = [];
+    }
 }
+
 
 module.exports = FSM;
 
